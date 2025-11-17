786064dcd680e792dbf3d4ce67604b0c2135b9e6
victorkane <victorkane@gmail.com>
2025-11-17 15:43:04 -0300

    content: initial personalization

--- src/docs/config/en/siteData.json.ts
+++ src/docs/config/en/siteData.json.ts
@@ -1,53 +1,58 @@
 import type { DocsSiteData } from "../types/configDataTypes";
 
 const docsSiteData: DocsSiteData = {
-	title: "Pathfinder",
+	title: "v. o. kapelman - writings",
 	description:
-		"A modern, fast, and feature-rich documentation theme built with Astro v5 and Tailwind CSS v4.",
+		"poetry, flash fiction, short stories, novels and other writings by v. o. kapelman",
 	navSocials: [
 		{
 			social: "X formerly known as Twitter",
-			link: "https://x.com/BowTiedWebReapr",
+			link: "https://x.com/victorkane",
 			icon: "tabler/brand-x",
 		},
 		{
+			social: "BlueSky",
+			link: "https://bsky.app/profile/vokapelman.bsky.social",
+			icon: "tabler/brand-bluesky",
+		},
+		{
 			social: "GitHub",
-			link: "https://github.com/Boston343",
+			link: "https://github.com/practicepoetica",
 			icon: "mdi/github",
 		},
 	],
 	footerSocials: [
 		{
 			social: "X formerly known as Twitter",
-			link: "https://x.com/BowTiedWebReapr",
+			link: "https://x.com/victorkane",
 			icon: "tabler/brand-x",
 		},
 		{
 			social: "Threads",
-			link: "https://www.threads.net/@bowtiedwebreaper",
+			link: "https://www.threads.com/@victorkane__",
 			icon: "tabler/brand-threads",
 		},
 		{
 			social: "BlueSky",
-			link: "https://bsky.app/profile/webreaper.dev",
+			link: "https://bsky.app/profile/vokapelman.bsky.social",
 			icon: "tabler/brand-bluesky",
 		},
 		{
 			social: "GitHub",
-			link: "https://github.com/Boston343",
+			link: "https://github.com/victorkane",
 			icon: "tabler/brand-github",
 		},
 	],
 	// default image for meta tags if the page doesn't have an image already
 	defaultImage: {
-		src: "/images/cosmic-themes-logo.png",
-		alt: "Cosmic Themes logo",
+		src: "/images/vokapelman-20251026-79.jpeg",
+		alt: "v. o. kapelman",
 	},
 	// Your information for SEO purposes
 	author: {
-		name: "Cosmic Themes",
-		email: "creator@cosmicthemes.com",
-		twitter: "Cosmic_Themes",
+		name: "v. o. kapelman",
+		email: "victork_ar@yahoo.com",
+		twitter: "PraxisPoetica",
 	},
 };
 
